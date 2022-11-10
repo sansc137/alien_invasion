615fc2b540cef9fd390a827f4bd00f067198be46
sans <310705045@qq.com>
2022-11-10 16:05:51 +0800

    修改背景色

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,9 @@ class Alien_invasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien_Invasion")
 
+        #设置背景色
+        self.bg_color = (230, 230,230)
+
     def run_game(self):
         while True:
             # 监视键盘和鼠标事件
